c2fb08217e5ed96b62dc1f0ee3565118867df22e
zhengwenting <799180026@qq.com>
2026-07-15 16:54:16 +0800

    测试调整代码

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
-import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
-import legacy from "@vitejs/plugin-legacy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'./',
   build: {
     sourcemap: 'hidden'
   },
@@ -27,19 +24,6 @@ export default defineConfig({
         ],
       },
     }),
-    traeBadgePlugin({
-      variant: 'dark',
-      position: 'bottom-right',
-      prodOnly: true,
-      clickable: true,
-      clickUrl: 'https://www.trae.ai/solo?showJoin=1',
-      autoTheme: true,
-      autoThemeTarget: '#root'
-    }), 
-    tsconfigPaths(),
-    legacy({
-      targets: ["ie>=11"],
-      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
-    }),
+    tsconfigPaths()
   ],
 })
